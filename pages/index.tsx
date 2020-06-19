3c59de94edfa3892c94ef9a6d2ddb3a88d48e525
nguyen duc <ictduc@gmail.com>
2020-06-19 17:17:57 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,80 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import {Grid, Theme} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+
+import {makeStyles, createStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    root: {
+        backgroundColor: theme.palette.common.white,
+    },
+    gridHeader: {
+        position: 'relative'
+    },
+    backgroundCover: {
+        // position: 'absolute',
+        width: '100%'
+    },
+    header: {
+        position: 'absolute',
+        width: '100%',
+        top: 0,
+        left: 0
+    },
+    logo: {
+        width: 165,
+    },
+    menuItem: {
+        color: 'white',
+    },
+    sloganSearch: {
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    cardFirst: {
+        height: 250,
+        margin: 16,
+    },
+}));
 
 export default function Index() {
-  return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography component="h1" gutterBottom>
-          Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+    const classes = useStyles();
+    return (
+        <Grid container>
+            <Grid item xs={12} className={classes.gridHeader}>
+                <img
+                    className={classes.backgroundCover}
+                    alt={''} src={'https://cdn1.tripoto.com/assets/2.3/img/home_banner.jpg'}/>
+                <div className={classes.header}>
+                    <div className={'row'}>
+                        <img src={'https://cdn1.tripoto.com/assets/2.3/img/logo/tripoto.svg'} className={classes.logo}
+                             alt={'logo'}/>
+                        <div>
+
+                                <Typography variant="h6" component="h6" className={classes.menuItem}>
+                                    Inspirations
+                                </Typography>
+                                <Typography variant="h6" component="h6" className={classes.menuItem}>
+                                    Inspirations
+                                </Typography>
+
+                        </div>
+                    </div>
+                    <Typography variant="h3" component="h1" className={classes.sloganSearch}>
+                        The Global Community of Travellers
+                    </Typography>
+                </div>
+            </Grid>
+
+            <Grid item xs={12} lg={10}>
+                <img
+                    src={'https://cdn1.tripoto.com/media/filter/tst/img/319486/Image/1586846041_1584363033_contests_2.png'}
+                    alt={''} className={classes.cardFirst}/>
+                <img
+                    src={'https://cdn1.tripoto.com/media/filter/tst/img/319486/Image/1586846041_1584363033_contests_2.png'}
+                    alt={''} className={classes.cardFirst}/>
+            </Grid>
+
+        </Grid>
+    );
 }
